49306690479eccd373d3a0210b4e0818886a099d
JavierQuisphe <javier.quisphe@alumnos.uneatlantico.es>
2026-02-07 11:38:07 +0100

    Creación de variables Se crearon las variables que se usarán en el código base

---
+++ evaluaciones/retos/reto002/pyEdlin.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class pyEdlin {
+    private static final int MAX_LINEAS = 10;
+    private static String[] lineas = new String[MAX_LINEAS];
+    private static int lineaActica = 1;
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+    }
+}
