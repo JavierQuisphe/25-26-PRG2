5d5fba3eb8700be966e401b3778c3b1ab4cc3484
JavierQuisphe <javier.quisphe@alumnos.uneatlantico.es>
2026-03-20 04:29:08 +0100

    Creacion de los constructores publicos y de variables

---
+++ evaluaciones/retos/reto004/Fraccion.java
@@ -0,0 +1,12 @@
+public class Fraccion {
+    private int numerador;
+    private int denominador;
+
+    public Fraccion (int numerador, int denominador) {}
+    public Fraccion (int numero){}
+    public Fraccion (Fraccion fraccion){}
+    public Fraccion(){}
+
+    
+
+}
