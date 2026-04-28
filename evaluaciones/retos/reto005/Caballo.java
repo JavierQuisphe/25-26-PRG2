a3f85ac78f7530fc7c8eebe51235a8fed484122e
JavierQuisphe <javier.quisphe@alumnos.uneatlantico.es>
2026-04-28 20:10:58 +0200

    creación de la clase Caballo

---
+++ evaluaciones/retos/reto005/Caballo.java
@@ -0,0 +1,9 @@
+public class Caballo {
+    private String nombre;
+    private int posicion;
+
+    public Caballo(String nombre) {
+        this.nombre = nombre;
+        this.posicion = 0;
+    }
+}    
